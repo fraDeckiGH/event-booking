f9025942f1bb964fa8273f66da1e66565021ebe0
fdc Windows <fradc16@gmail.com>
2020-11-14 16:59:49 +0100

    master: #3 Schemas & Resolvers √

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,59 @@
-import express, { Application } from "express";
+import express, { Application, json } from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
 
 
 const app: Application = express();
 export default app;
+
+
+// =======================================================================
+// middleware (only belong before the routes)
+
+// bodyParser
+// what we'll be able to find in APIs' req.body
+app.use(
+	json(),
+	// urlencoded({ extended: false }),
+);
+
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+			type RootQuery {
+				events: [String!]!
+			}
+			
+			type RootMutation {
+				createEvent(name: String): String
+			}
+			
+			schema {
+				query: RootQuery
+				mutation: RootMutation
+			}
+    `),
+    rootValue: {
+      events: () => {
+        return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+      },
+      createEvent: (args: any) => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
+
+
+
+
+
+
+
+
+
+
