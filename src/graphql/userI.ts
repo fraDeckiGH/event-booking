c10291539454a735f17360ac2cca09ee6d92f9d1
De Chirico Francesco <22662277+fraDeckiGH@users.noreply.github.com>
2021-05-13 23:09:34 +0200

    typegraphql: adding mixins

--- src/graphql/userI.ts
+++ src/graphql/userI.ts
@@ -1,12 +1,33 @@
-import { InterfaceType, Field, ID } from "type-graphql"
+import { InterfaceType, Field, ID, InputType, ObjectType, ClassType } from "type-graphql"
 import { EmailAddress, NonEmptyString, Timestamp } from "../junction/scalar"
 import NodeI from "./node"
 
-@InterfaceType()
-export default abstract class UserI {
+// @InterfaceType()
+// export default abstract class UserI {
+//   @Field(type => EmailAddress)
+//   email!: string
+  
+//   @Field(type => NonEmptyString, { nullable: true })
+//   nickname?: string
+// }
+
+@ObjectType({ isAbstract: true })
+@InputType("UserBaseInput", { isAbstract: true })
+export default class UserBase {
   @Field(type => EmailAddress)
   email!: string
   
   @Field(type => NonEmptyString, { nullable: true })
   nickname?: string
+}
+
+/* export default  */
+function withId<TClassType extends ClassType>(BaseClass: TClassType) {
+  @ObjectType({ isAbstract: true })
+  @InputType({ isAbstract: true })
+  class IDTrait extends BaseClass {
+    @Field(type => Int)
+    id!: number;
+  }
+  return IDTrait;
 }
