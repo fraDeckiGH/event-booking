a6745ea61b9ad7ecc1058ced7e68b8361352fc6c
De Chirico Francesco <22662277+fraDeckiGH@users.noreply.github.com>
2021-07-16 19:46:20 +0200

    deno: unfinished setup

--- deps.ts
+++
@@ -1,3 +0,0 @@
-export * from "https://cdn.skypack.dev/fastify@^3.18.1?dts"
-export * from "https://cdn.skypack.dev/mercurius@^7.9.1?dts"
-export * from "https://cdn.skypack.dev/type-graphql@^1.1.1"
